96825df4890b84d0bc820494cf3b152b1a7ca2d7
Mukarram Kazmi <muazzam.ali.kazmi@gmail.com>
2022-11-30 19:41:00 +0500

    Added whatsapp link

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -15,22 +15,29 @@ const ProductCard = ({ product }) => {
                 <span className='price'>Price: Rs. {price}</span>
             </div>
             <div className='footer'>
-                <span className='price'>Down payment: Rs. {(price * 20 / 100).toFixed(2)}</span>
+                <span className='price'>Down payment: Rs. {(price * 20 / 100).toFixed(0)}</span>
             </div>
             <div className='footer'>
-                <span className='price'>12 Month Plan: Rs. {((price - ((price * 20) / 100)) / 11).toFixed(2)}</span>
+                <span className='price'>12 Month Plan: Rs. {((price - ((price * 20) / 100)) / 11).toFixed(0)}</span>
             </div>
             <div className='footer'>
-                <span className='price'>24 Month Plan: Rs. {((price - ((price * 20) / 100)) / 23).toFixed(2)}</span>
+                <span className='price'>24 Month Plan: Rs. {((price - ((price * 20) / 100)) / 23).toFixed(0)}</span>
             </div>
             <div className='footer'>
-                <span className='price'>36 Month Plan: Rs. {((price - ((price * 20) / 100)) / 35).toFixed(2)}</span>
+                <span className='price'>36 Month Plan: Rs. {((price - ((price * 20) / 100)) / 35).toFixed(0)}</span>
             </div>
             <div className='footer'>
-                <div className='whatsapp-container'>
-                    <Whatsapp className='whatsapp' />
-                    +92 321 1234567
-                </div>
+                <a
+                    href="https://wa.me/923057110967"
+                    class="whatsapp-link"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <div className='whatsapp-container'>
+                        <Whatsapp className='whatsapp' />
+                        +92 305 7110967
+                    </div>
+                </a>
             </div>
         </div>  
     );
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -3,7 +3,7 @@ import { Outlet, Link } from 'react-router-dom';
 
 import { UserContext } from '../../contexts/user.context';
 
-import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+import { ReactComponent as Logo } from '../../assets/logo.svg';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 
 import './navigation.styles.scss'
@@ -15,7 +15,7 @@ const Nevigation = () => {
         <>
             <div className='navigation'>
                 <Link className='logo-container' to='/'>
-                    <CrwnLogo className='logo' />
+                    <Logo className='logo' />
                 </Link>
                 <div className='nav-links-container'>
                     {
