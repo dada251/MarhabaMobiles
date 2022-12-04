335fba19291414e833aeb0430a58be6f664b27f0
dada251 <119763141+dada251@users.noreply.github.com>
2022-12-05 00:04:46 +0500

    Update product-card.component.jsx

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,5 +1,6 @@
 import './product-card.styles.scss';
 
+
 import { ReactComponent as Whatsapp } from '../../assets/whatsapp.svg';
 
 const ProductCard = ({ product }) => {
@@ -9,36 +10,66 @@ const ProductCard = ({ product }) => {
         <div className='product-card-container'>
             <img src={imageUrl} alt={name} />
             <div className='footer'>
-                <span className='name'>Name: {name}</span>
+                <span className='name'>{name} </span>
             </div>
             <div className='footer'>
-                <span className='price'>Price: Rs. {price}</span>
+                <span className='price'>Rs. {price} only</span>
             </div>
             <div className='footer'>
                 <h2  className='name'>Easy Installment Plan</h2>
-                <span className='price'>Down payment: Rs. {(price * 20 / 100).toFixed(0)}</span>
+                <span className='price'><span className='down-payment' >Down payment:</span>Rs. {(price * 20 / 100).toFixed(0)}</span>
             </div>
             <div className='footer'>
-                <span className='price'>12 Month Plan: Rs. {((price - ((price * 20) / 100)) / 11).toFixed(0)} /per month</span>
-            </div>
+                <span className='price'><span className='bg'>12 Month Plan: </span>Rs. {((price - ((price * 20) / 100)) / 11).toFixed(0)} /per month</span>
+            </div><br/>
             <div className='footer'>
-                <span className='price'>24 Month Plan: Rs. {((price - ((price * 20) / 100)) / 23).toFixed(0)} /per month</span>
-            </div>
+                <span className='price'><span className='bg'>24 Month Plan:</span> Rs. {((price - ((price * 20) / 100)) / 23).toFixed(0)} /per month</span>
+            </div><br/>
             <div className='footer'>
-                <span className='price'>36 Month Plan: Rs. {((price - ((price * 20) / 100)) / 35).toFixed(0)} /per month</span>
+                <span className='price'><span className='bg'>36 Month Plan: </span>Rs. {((price - ((price * 20) / 100)) / 35).toFixed(0)} /per month</span>
             </div>
             <div className='footer'>
                 <a
-                    href="https://wa.me/923057110967"
+                    href="https://wa.me/923258199119"
                     class="whatsapp-link"
                     target="_blank"
                     rel="noopener noreferrer"
                 >
                     <div className='whatsapp-container'>
                         <Whatsapp className='whatsapp' />
-                        +92 305 7110967
+                        Buy Now
                     </div>
                 </a>
+                
+            </div>
+
+            <div className='footer'>
+                <a
+                    href="https://wa.me/923268672603"
+                    class="whatsapp-link"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <div className='whatsapp-container'>
+                        <Whatsapp className='whatsapp' />
+                        Ask for Details
+                    </div>
+                </a>
+                
+            </div>
+            <div className='footer'>
+                <a
+                    href="https://wa.me/923267647251"
+                    class="whatsapp-link"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <div className='whatsapp-container'>
+                        <Whatsapp className='whatsapp' />
+                        contact Us
+                    </div>
+                </a>
+                
             </div>
         </div>  
     );
